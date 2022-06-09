0e99352e0dd14ea5d6ebb8ce2ad346002ad4599f
Patrícia Campos <patricia.r.cam@gmail.com>
2022-06-09 20:35:17 -0300

    Inseri um pedido de socorro

--- src/main/java/ExemplosAula/ExercicioStringPalavra.java
+++ src/main/java/ExemplosAula/ExercicioStringPalavra.java
@@ -4,6 +4,7 @@ public class ExercicioStringPalavra {
 
     public static void main(String[] args) {
         imprimeCadaPalavra("A pressa é a inimiga da perfeição");
+        System.out.println("Socorro");
     }
 
     public static void imprimeCadaPalavra(String texto) {
