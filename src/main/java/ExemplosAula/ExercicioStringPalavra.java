bcb2978245525884d70bd1e5f4b94cb0372e3229
Patrícia Campos <patricia.r.cam@gmail.com>
2022-06-09 20:32:23 -0300

    Inserindo começo de novo exercício

---
+++ src/main/java/ExemplosAula/ExercicioStringPalavra.java
@@ -0,0 +1,15 @@
+package ExemplosAula;
+
+public class ExercicioStringPalavra {
+
+    public static void main(String[] args) {
+        imprimeCadaPalavra("A pressa é a inimiga da perfeição");
+    }
+
+    public static void imprimeCadaPalavra(String texto) {
+
+
+    }
+
+
+}
